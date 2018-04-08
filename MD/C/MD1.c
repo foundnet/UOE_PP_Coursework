10263c6873853efd67e0ee1fdd99d2c4e48e8c97
foundnet <foundnet@hotmail.com>
2018-04-08 02:33:05 -0400

    sdfsf
    sdf

--- MD/C/MD1.c
+++ MD/C/MD1.c
@@ -77,14 +77,13 @@ void evolve(int count, double dt)
     for (l = 0; l < Ndim; l++)
     {
       k = 0;
-
       for (i = 0; i < Nbody; i++)
       {
         for (j = i + 1; j < Nbody; j++)
         {
           delta_pos[l][k] = pos[l][i] - pos[l][j];
+          k = k + 1;
         }
-        k = k + 1;
       }
     }
 
